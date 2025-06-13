64e0aefe93f376c55b136f6ff5abaac382ceac67
IanFeugeas <ianfabricio1889@gmail.com>
2025-06-13 09:48:21 -0300

    login models

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,31 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
 
     nombre: { type: String, required: true},
+    email : { type: String, required: true, unique: true},
+    password: { type: String, required: true },
+    rol: { type: String, enum: ['alumno', 'profesor', 'admin'], default: 'alumno'}
+});
+
+// Hashear la contraseña antes de guardar
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Metodo para validar la contraseña
+
+userSchema.methods.compararPassword = function (passwordIngresada) {
+    return bcrypt.compare(passwordIngresada, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+
+    nombre: { type: String, required: true},
     email: {
         type: String,
         required: true,
