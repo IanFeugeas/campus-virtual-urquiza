f361c6fbcbb2bc3eed9dc649bf309063fb69dbb9
IanFeugeas <ianfabricio1889@gmail.com>
2025-06-13 10:20:43 -0300

    User modificado

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('API funcionando 👋');
 });
+
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/foro';
