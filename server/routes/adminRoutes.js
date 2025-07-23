04bb24f817dbe8c2999757355f9cd700eef9fc19
IanFeugeas <ianfabricio1889@gmail.com>
2025-07-22 23:53:31 -0300

    Admin general creado

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const verificarToken = require('../middlewares/verificarToken');
+const verificarToken = require('../middlewares/authMiddleware');
 const verificarRol = require('../middlewares/verificarRol');
 const { obtenerUsuarios } = require('../controllers/adminController');
 
