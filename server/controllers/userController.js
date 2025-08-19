877ba49adda442a42b83411508aca906ded3731c
IanFeugeas <ianfabricio1889@gmail.com>
2025-08-19 00:37:10 -0300

    Modificar usuario

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,7 +34,39 @@ const obtenerUsuarioPorId = async (req, res) => {
   }
 };
 
+const actualizarUsuario = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (req.usuario.id !== id && req.usuario.rol !== 'admin') {
+      return res.status(403).json({ mensaje: 'No tenes permiso para modificar los datos de este perfil'});
+    }
+
+    const { nombre, carrera } = req.body;
+
+    const usuario = await User.findByIdAndUpdate(
+      id,
+      { nombre, carrera },
+      { new: true, runValidators: true } 
+    ).select('-password');
+
+    if (!usuario) {
+      return res.status(404).json({ mensaje: 'Usuario no encontrado'});
+    }
+
+    res.status(200).json({
+      mensaje: 'Usuario actualizado correctamente',
+      usuario,
+    });
+
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al actualizar el usuario' });
+  }
+};
+
 module.exports = {
     obtenerPerfil,
-    obtenerUsuarioPorId
+    obtenerUsuarioPorId,
+    actualizarUsuario
     };
